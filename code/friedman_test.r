9fd4a7cd2891ff990d66626e37e37fd1c4b2cd80
Anna Truzzi <46749720+AnnaTruzzi@users.noreply.github.com>
2022-09-22 10:49:19 +0100

    friedman test hcp on R

--- code/friedman_test.r
+++ code/friedman_test.r
@@ -1,5 +1,5 @@
 #setwd('/dhcp/fmri_anna_graham/dhcp_camcan_timescales/results/')
-setwd('C:/Users/Anna/Documents/Research/Projects/ONGOING/Project dHCP_Autocorrelation/dhcp_camcan_timescales/results')
+setwd('C:/Users/Anna/Documents/Research/Projects/ONGOING/Project dHCP_Autocorrelation/dhcp_hcp_timescales/results')
 
 ### dhcp_group1
 db <- read.csv('tau_estimation_dhcp_group1.csv', sep=',')
@@ -12,7 +12,6 @@ friedman.test(as.matrix(db_tau))
 
 ### dhcp_group2
 db <- read.csv('tau_estimation_dhcp_group2.csv', sep=',')
-friedman.test(as.matrix(db[,4:403]))
 db_tau <- db[,4:403]
 p95 <- quantile(db_tau,.95,na.rm=TRUE)
 db_tau[db_tau>p95] <- NA
@@ -21,8 +20,7 @@ friedman.test(as.matrix(db_tau))
 
 ### dhcp_group2
 db <- read.csv('tau_estimation_hcp.csv', sep=',')
-friedman.test(as.matrix(db[,4:403]))
-db_tau <- db[,4:403]
+db_tau <- db[,3:402]
 p95 <- quantile(db_tau,.95,na.rm=TRUE)
 db_tau[db_tau>p95] <- NA
 friedman.test(as.matrix(db_tau))
