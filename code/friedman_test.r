94827045b186f273875449d72642dd9eb6aa7286
Anna Truzzi <46749720+AnnaTruzzi@users.noreply.github.com>
2022-11-09 18:40:00 +0000

    brain renders and paper figures

--- code/friedman_test.r
+++ code/friedman_test.r
@@ -2,7 +2,7 @@
 setwd('C:/Users/Anna/Documents/Research/Projects/ONGOING/Project dHCP_Autocorrelation/dhcp_hcp_timescales/results')
 
 ### dhcp_group1
-db <- read.csv('tau_estimation_dhcp_group1.csv', sep=',')
+db <- read.csv('tau_estimation_dhcp_group1_7net.csv', sep=',')
 db_tau <- db[,4:403]
 p95 <- quantile(db_tau,.95,na.rm=TRUE)
 db_tau[db_tau>p95] <- NA
@@ -11,7 +11,7 @@ friedman.test(as.matrix(db_tau))
 
 
 ### dhcp_group2
-db <- read.csv('tau_estimation_dhcp_group2.csv', sep=',')
+db <- read.csv('tau_estimation_dhcp_group2_7net.csv', sep=',')
 db_tau <- db[,4:403]
 p95 <- quantile(db_tau,.95,na.rm=TRUE)
 db_tau[db_tau>p95] <- NA
@@ -19,7 +19,7 @@ friedman.test(as.matrix(db_tau))
 
 
 ### dhcp_group2
-db <- read.csv('tau_estimation_hcp.csv', sep=',')
+db <- read.csv('tau_estimation_hcp_7net.csv', sep=',')
 db_tau <- db[,3:402]
 p95 <- quantile(db_tau,.95,na.rm=TRUE)
 db_tau[db_tau>p95] <- NA
