22fa10d66b035595d0bd490cf8410d1e58993477
Anna Truzzi <46749720+AnnaTruzzi@users.noreply.github.com>
2023-03-13 16:44:33 +0000

    Update friedman_test.r

--- code/friedman_test.r
+++ code/friedman_test.r
@@ -18,9 +18,28 @@ db_tau[db_tau>p95] <- NA
 friedman.test(as.matrix(db_tau))
 
 
-### dhcp_group2
+### hcp
 db <- read.csv('tau_estimation_hcp_7net.csv', sep=',')
 db_tau <- db[,3:402]
 p95 <- quantile(db_tau,.95,na.rm=TRUE)
 db_tau[db_tau>p95] <- NA
 friedman.test(as.matrix(db_tau))
+
+
+
+### dhcp_group1 drop_scan
+db <- read.csv('tau_estimation_dhcp_group1_7net_drop_scan_dhcp.csv', sep=',')
+db_tau <- db[,4:403]
+p95 <- quantile(db_tau,.95,na.rm=TRUE)
+db_tau[db_tau>p95] <- NA
+friedman.test(as.matrix(db_tau))
+
+
+
+### dhcp_group2_drop_scan
+db <- read.csv('tau_estimation_dhcp_group2_7net_drop_scan_dhcp.csv', sep=',')
+db_tau <- db[,4:403]
+p95 <- quantile(db_tau,.95,na.rm=TRUE)
+db_tau[db_tau>p95] <- NA
+friedman.test(as.matrix(db_tau))
+
